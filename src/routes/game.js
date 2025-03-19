efeec9b0b4ec7c2be2b7db2a7e1daf331af39fd1
Brendan <brendanlawless@hotmail.com>
2025-03-19 11:43:25 +0000

    refactor: move uuid assignment to controllers/game.js

--- src/routes/game.js
+++ src/routes/game.js
@@ -1,15 +1,9 @@
 import { Router } from "express";
-import { v4 as uuidv4 } from "uuid";
 import { startGame, checkGuess } from "../controllers/game.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  if (!req.session.playerId) {
-    req.session.playerId = uuidv4();
-  }
-  startGame(req, res);
-});
+router.get("/", startGame);
 
 router.post("/guess", checkGuess);
 
