853a83302b9371c468a389a4b3fe12343da474ca
Brendan <brendanlawless@hotmail.com>
2025-03-19 11:36:58 +0000

    refactor: remove all logic that doesn't relate to characters coordinates have been hit
    - remove timer, foundArr and gameOver logic
    - add unique playerId in ejs data.

--- src/controllers/game.js
+++ src/controllers/game.js
@@ -1,55 +1,33 @@
-import { puzzles, gameState, GAME_DURATION } from "../models/puzzles.js";
+import { puzzles } from "../models/puzzles.js";
+import { v4 as uuidv4 } from "uuid";
 
 export const startGame = (req, res) => {
-    const sessionId = req.headers["x-session-id"] || "default";
-    gameState.set(sessionId, { startTime: Date.now(), found: new Set()});
-    res.render("index", {
-        puzzles:  Object.values(puzzles).map(char => char.img),
-        playerId: req.session.playerId,
-        initialTime: GAME_DURATION / 1000,
-    });
+  res.render("index", {
+    puzzles: Object.values(puzzles).map((char) => char.img),
+    playerId: req.session.playerId = uuidv4(),
+  });
 };
 
 export const checkGuess = (req, res) => {
-    const { index, x, y } = req.body;
-    const sessionId = req.headers["x-session-id"] || "default";
-    const state = gameState.get(sessionId);
-    const elapsed = Date.now() - state.startTime;
-    const timeLeft = Math.max(0, (GAME_DURATION - elapsed) / 1000);
-    const characters = puzzles[index].characters;
+  const { index, x, y } = req.body;
+  const characters = puzzles[index].characters;
 
-    if (timeLeft <= 0) {
-        return res.json({
-            success: false,
-            message: "Times Up!",
-            timeLeft: 0,
-            timeTaken: null,
-        });
+  let solved = false;
+
+  for (let character in characters) {
+    const inRange =
+      x >= characters[character].x &&
+      x <= characters[character].x + characters[character].width &&
+      y >= characters[character].y &&
+      y <= characters[character].y + characters[character].height;
+
+    if (inRange && character === "waldo") {
+      solved = true;
+      break;
     }
+  }
 
-    let solved = false;
-
-
-    for (let character in characters) {
-        const inRange =
-            x >= characters[character].x &&
-            x <= characters[character].x + characters[character].width &&
-            y >= characters[character].y &&
-            y <= characters[character].y + characters[character].height;
-
-        if (inRange && character === 'waldo') {
-            state.found.add(index);
-            solved = true;
-        }
-
-    }
-
-    const allFound = state.found.size === Object.keys(puzzles).length;
-    const timeTaken = allFound ? elapsed / 1000 : null;
-
-    res.json({
-        success: solved,
-        timeTaken,
-        timeLeft,
-    });
+  res.json({
+    success: solved,
+  });
 };
