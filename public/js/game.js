b566789266dfa074ed83969815cfd64c283d3ae6
Brendan <brendanlawless@hotmail.com>
2025-03-25 13:42:38 +0000

    refactor: remove redundant and streamline code
    - http response no longer used as server now signals updates with WS
    - setter functions made into oneliners

--- public/js/game.js
+++ public/js/game.js
@@ -1,96 +1,51 @@
-import { allPuzzles, mainPuzzle, timerDisplay } from "./main.js";
-import {
-  updateTimerDisplay,
-  switchToUnsolvedPuzzle,
-  updateThumbnails,
-} from "./ui.js";
+import {  timerDisplay } from "./main.js";
+import { updateTimerDisplay, } from "./ui.js";
 
 let gameOver, startTime;
 export let foundArr = [];
 export let powerUpsArr = [];
 
-export function setFoundArr(newArr) {
-  foundArr = newArr;
-}
-
-export function setPowerUpsArr(newArr) {
-  powerUpsArr = newArr;
-}
-
-export function setGameOver() {
-  gameOver = true;
-}
-
-export function setStartTime(time) {
-  startTime = time;
-}
+export const setFoundArr = (newArr) => foundArr = newArr;
+export const setPowerUpsArr = (newArr) => powerUpsArr = newArr;
+export const setGameOver = () => gameOver = true;
+export const setStartTime = (time) => startTime = time;
 
 export function startGame() {
-  gameOver = false;
-  const totalTime = 300000;
+    gameOver = false;
+    const totalTime = 300000;
 
-  const timerInterval = setInterval(() => {
-    if (gameOver) {
-      clearInterval(timerInterval);
-      return;
-    }
+    const timerInterval = setInterval(() => {
+        if (gameOver) {
+            clearInterval(timerInterval);
+            return;
+        }
 
-    let timeLeft = totalTime - (Date.now() - startTime);
-    if (timeLeft <= 0) {
-      clearInterval(timerInterval);
-      setGameOver();
-      updateTimerDisplay("Time's Up!", timerDisplay);
-      return;
-    }
+        let timeLeft = totalTime - (Date.now() - startTime);
+        if (timeLeft <= 0) {
+            clearInterval(timerInterval);
+            setGameOver();
+            updateTimerDisplay("Time's Up!", timerDisplay);
+            return;
+        }
 
-    const minutes = Math.floor(timeLeft / 60000);
-    const seconds = Math.floor((timeLeft % 60000) / 1000)
-      .toString()
-      .padStart(2, "00");
-    updateTimerDisplay(`Time left: ${minutes}:${seconds}`, timerDisplay);
-  }, 1000);
+        const minutes = Math.floor(timeLeft / 60000);
+        const seconds = Math.floor((timeLeft % 60000) / 1000)
+            .toString()
+            .padStart(2, "00");
+
+        updateTimerDisplay(`Time left: ${minutes}:${seconds}`, timerDisplay);
+    }, 1000);
 }
 
-export async function checkCharacter(puzzleIdx, x, y, ws, playerId) {
-  try {
-    const res = await fetch("/guess", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ puzzleIdx, x, y }),
-    });
-
-    const data = await res.json();
-
-    const { charFound } = data;
-
-    if (charFound === "waldo") {
-      foundArr[puzzleIdx] = true;
-      updateThumbnails(allPuzzles);
-      switchToUnsolvedPuzzle(mainPuzzle, puzzles, foundArr);
-      ws.send(JSON.stringify({ type: "updateFound", foundArr, playerId }));
-    } else if (charFound) {
-
-        powerUpsArr[puzzleIdx][charFound] = true;
-
-        switch(charFound) {
-            case "odlaw":
-                console.log(`${charFound} has been found in puzzle ${puzzleIdx}`);
-                ws.send(JSON.stringify({type: "powerUpFound", powerUpsArr, playerId, character: charFound}))
-                break;
-            case "wenda":
-                console.log(`${charFound} has been found in puzzle ${puzzleIdx}`);
-                ws.send(JSON.stringify({type: "powerUpFound", powerUpsArr, playerId, character: charFound}))
-                break;
-            case "whitebeard":
-                console.log(`${charFound} has been found in puzzle ${puzzleIdx}`);
-                ws.send(JSON.stringify({type: "powerUpFound", powerUpsArr, playerId, character: charFound}))
-                break;
-            default:
-                console.log(`Unknown character ${charFound}`);
-        }
+export async function checkCharacter(puzzleIdx, x, y, playerId) {
+    try {
+         await fetch("/guess", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ puzzleIdx, x, y, playerId }),
+        });
+    } catch (err) {
+        console.error("Fetch error: ", err);
+        alert("Something went wrong");
     }
-  } catch (err) {
-    console.error("Fetch error: ", err);
-    alert("Something went wrong");
-  }
 }
